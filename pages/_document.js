2e6bcd84301cbb8c141d7e8dcab8bb80516f0baf
brunoleo223 <brunoleo223@gmail.com>
2022-09-11 00:26:04 -0300

    test with next script

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-sync-scripts */
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -9,6 +10,10 @@ export default function Document() {
         <Main />
         <NextScript />
         <script src="https://unpkg.co/gsap/dist/gsap.min.js"></script>
+        <Script
+          src="main.js"
+          strategy="lazyOnload"
+        />
       </body>
     </Html>
   )
--- pages/index.js
+++ pages/index.js
@@ -7,58 +7,6 @@ import styles from '../styles/index.module.scss'
 */
 
 export default function Home({data}) {
-
-
-  const allVenues = gsap.utils.toArray(".venues-item");
-  const venueImageWrap = document.querySelector(".venues-img-wrap");
-  const venueImage = document.querySelector(".venues-img");
-
-  function initVenues() {
-    allVenues.forEach((link) => {
-      link.addEventListener("mouseenter", venueHover);
-      link.addEventListener("mouseleave", venueHover);
-      link.addEventListener("mousemove", moveVenueImage);
-    });
-  }
-
-  function moveVenueImage(e) {
-    let xpos = e.clientX;
-    let ypos = e.clientY;
-    const tl = gsap.timeline();
-    tl.to(venueImageWrap, {
-      x: xpos,
-      y: ypos
-    });
-  }
-
-  function venueHover(e) {
-    if (e.type === "mouseenter") {
-      const targetImage = e.target.dataset.img;
-
-      const t1 = gsap.timeline();
-      t1.set(venueImage, {
-        backgroundImage: `url(${targetImage})`
-      }).to(venueImageWrap, {
-        duration: 0.5,
-        autoAlpha: 1
-      });
-    } else if (e.type === "mouseleave") {
-      const tl = gsap.timeline();
-      tl.to(venueImageWrap, {
-        duration: 0.5,
-        autoAlpha: 0
-      });
-    }
-  }
-
-  function init() {
-    initVenues();
-  }
-
-  init();
-
-
-
   const options = {  year: 'numeric', month: 'long', day: 'numeric' };
   return (
     <>
