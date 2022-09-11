ca0217c0a610cb2b1470ac0db659f7ec843d9847
brunoleo223 <brunoleo223@gmail.com>
2022-09-11 00:19:48 -0300

    initial

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script src="https://unpkg.co/gsap/dist/gsap.min.js"></script>
+      </body>
+    </Html>
+  )
+}
